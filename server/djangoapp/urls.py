59c842e53c3bdffaa50c68f6ecdab850e96d4a0a
Une Nyide <une.nyide@gmail.com>
2024-02-10 10:05:03 -0500

    add about route & page

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -8,12 +8,14 @@ urlpatterns = [
     # route is a string contains a URL pattern
     # view refers to the view function
     # name the URL
-
-    # path for about view
+    path(route="static", view=views.static_page, name="static_page"),
     
-
+    # path for about view
+    path(route="about", view=views.about, name="about"),
+    
     # path for contact us view
-
+    path(route="contact", view=views.contact, name="contact"),
+    
     # path for registration
 
     # path for login
