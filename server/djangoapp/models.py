704e7de0342a08862860f0acd281b516be5f8d99
Une Nyide <une.nyide@gmail.com>
2024-02-11 08:09:19 -0500

    add carmake model

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -2,14 +2,12 @@ from django.db import models
 from django.utils.timezone import now
 
 
-# Create your models here.
+class CarMake(models.Model):
+    name = models.CharField(null=False, max_length=30)
+    description = models.CharField(max_length=300, default="No description available")
 
-# <HINT> Create a Car Make model `class CarMake(models.Model)`:
-# - Name
-# - Description
-# - Any other fields you would like to include in car make model
-# - __str__ method to print a car make object
-
+    def __str__(self):
+        return f"Make: {self.name}, Description: {self.description}"
 
 # <HINT> Create a Car Model model `class CarModel(models.Model):`:
 # - Many-To-One relationship to Car Make model (One Car Make has many Car Models, using ForeignKey field)
