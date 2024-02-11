41b1e17a1f26016ebefc96e9baa853e54ab32c91
Une Nyide <une.nyide@gmail.com>
2024-02-11 03:40:49 -0500

    add logout view

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -55,7 +55,8 @@ def login_request(request):
 
 # Create a `logout_request` view to handle sign out request
 def logout_request(request):
-    
+    logout(request)
+    return redirect("djangoapp:index")
 
 
 # Create a `registration_request` view to handle sign up request
